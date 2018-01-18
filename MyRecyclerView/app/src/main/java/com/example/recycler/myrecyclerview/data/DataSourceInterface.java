05db222304e5295e3e8af4a38a4feba9695cbe01
Kelly Bentes <kellymaria.bentes@gmail.com>
2018-01-18 12:29:53 -0200

    Codigo em java usando RecyclerView e desaclopamento simples

---
+++ MyRecyclerView/app/src/main/java/com/example/recycler/myrecyclerview/data/DataSourceInterface.java
@@ -0,0 +1,13 @@
+package com.example.recycler.myrecyclerview.data;
+
+import java.util.List;
+
+/**
+ * Created by kellybentes on 15/01/18.
+ */
+
+public interface DataSourceInterface {
+
+    List<ListItem> getListOfData();
+
+}
---
+++ MyRecyclerView/app/src/main/java/com/example/recycler/myrecyclerview/data/ListItem.java
@@ -0,0 +1,42 @@
+package com.example.recycler.myrecyclerview.data;
+
+/**
+ * Created by kellybentes on 15/01/18.
+ */
+
+public class ListItem {
+
+    private String dateAndTime;
+    private String message;
+    private int colorResource;
+
+    public ListItem(String dateAndTime, String message, int colorResource) {
+        this.dateAndTime = dateAndTime;
+        this.message = message;
+        this.colorResource = colorResource;
+    }
+
+    public String getDateAndTime() {
+        return dateAndTime;
+    }
+
+    public void setDateAndTime(String dateAndTime) {
+        this.dateAndTime = dateAndTime;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public int getColorResource() {
+        return colorResource;
+    }
+
+    public void setColorResource(int colorResource) {
+        this.colorResource = colorResource;
+    }
+}
