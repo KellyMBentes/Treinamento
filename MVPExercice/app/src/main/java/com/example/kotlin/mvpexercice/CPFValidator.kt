acf7892ad912d88defb654d91776669557a4cc54
KellyMBentes <kellymaria.bentes@gmail.com>
2018-02-21 13:23:49 -0300

    Ajuste da blacklist do cpf validator

--- MVPExercice/app/src/main/java/com/example/kotlin/mvpexercice/CPFValidator.kt
+++ MVPExercice/app/src/main/java/com/example/kotlin/mvpexercice/CPFValidator.kt
@@ -6,7 +6,8 @@ package com.example.kotlin.mvpexercice
 
 class CPFValidator (){
 
-    private val cpfBlacklist = listOf("00000000000")
+    private val cpfBlacklist = listOf("00000000000", "11111111111", "22222222222", "33333333333",
+            "44444444444", "55555555555", "66666666666","77777777777", "88888888888", "99999999999")
 
     fun validate (cpf : String) : Boolean {
 
