a701725eb393bdd74d8de97b32a77c4ae28ad6d1
KellyMBentes <kellymaria.bentes@gmail.com>
2018-02-27 18:19:50 -0300

    Ajustes nos tests do presenter

--- MVPExercice/app/src/test/java/com/example/kotlin/mvpexercice/SubscriptionFormPresenterUnitTest.kt
+++ MVPExercice/app/src/test/java/com/example/kotlin/mvpexercice/SubscriptionFormPresenterUnitTest.kt
@@ -1,41 +1,49 @@
 package com.example.kotlin.mvpexercice
 
+import com.nhaarman.mockitokotlin2.mock
 import org.junit.Before
 import org.junit.Test
-import org.mockito.Mock
-import org.mockito.Mockito
-import org.mockito.Mockito.mock
+import com.nhaarman.mockitokotlin2.verify
 
 class SubscriptionFormPresenterUnitTest {
 
-    @Mock
     private lateinit var subscriptionFormView: SubscriptionFormView
     private lateinit var subscriptionFormPresenter: SubscriptionFormPresenter
 
     @Before
     fun `before each test` (){
+        subscriptionFormView = mock()
         subscriptionFormPresenter = SubscriptionFormPresenter(subscriptionFormView, NameValidator(), CPFValidator(), EmailValidator())
-        subscriptionFormView = mock(SubscriptionFormView::class.java)
     }
 
     @Test
     fun `should call name invalid when the changed name is invalid` (){
         subscriptionFormPresenter.userNameChanged("")
 
-        Mockito.verify(subscriptionFormView).onNameInvalid()
+        verify(subscriptionFormView).onNameInvalid()
+        verify(subscriptionFormView).notReadyToValidate()
     }
 
     @Test
     fun `should call cpf invalid when the changed cpf is invalid` (){
         subscriptionFormPresenter.userCPFChanged("")
 
-        Mockito.verify(subscriptionFormView).onCPFInvalid()
+        verify(subscriptionFormView).onCPFInvalid()
+        verify(subscriptionFormView).notReadyToValidate()
     }
 
     @Test
     fun `should call email invalid when the changed email is invalid` (){
         subscriptionFormPresenter.userEmailChanged("")
 
-        Mockito.verify(subscriptionFormView).onEmailInvalid()
+        verify(subscriptionFormView).onEmailInvalid()
+        verify(subscriptionFormView).notReadyToValidate()
+    }
+
+    @Test
+    fun `should call validation dialog when perform validation` (){
+        subscriptionFormPresenter.performValidation()
+
+        verify(subscriptionFormView).showValidationDialog()
     }
 }
